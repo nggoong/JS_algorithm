ffb4aa908ae3061ca23fd76a0c9ad788ae4958ee
nggoong <nggoong@naver.com>
2022-03-31 10:37:39 +0900

    뒤집은 소수 문제 커밋✔

---
+++ BruteForce/ReversePrime.js
@@ -0,0 +1,34 @@
+// 뒤집은 소수
+
+(()=> {
+    function isPrime(num) {
+        if(num === 1) return false;
+        else if(num === 2) return true;
+        
+        for(let i = 2; i < num / 2; i++) {
+            if(num % i === 0) return false;
+        }
+        
+        return true;
+    }
+
+    function solution(n, arr) {
+        let answer=[];
+        let temp;
+        
+        for(let i = 0; i < n; i++) {
+            temp = Number(String(arr[i]).split('').reverse().join(''));
+            console.log(temp);
+            
+            if(isPrime(temp)) {
+                answer.push(temp);
+            }
+    }
+    return answer;
+}
+
+    console.log(solution(9, [32,55,62,20,250,370,200,30,100]));
+})();
+
+
+
