800a62b5e7e7ef2288edc8745b9d0b125326d543
nggoong <nggoong@naver.com>
2022-04-02 14:47:32 +0900

    K번째 수 문제 커밋✔

---
+++ BruteForce/KthOfNumber.js
@@ -0,0 +1,21 @@
+(()=> {
+    function solution(n, k , card) {
+        let answer;
+        let temp = new Set();
+
+        for(let i = 0; i< n ; i++) {
+            for(let j = i+1; j < n; j++) {
+                for(let k = j+1; k < n; k++) {
+                    temp.add(card[i] + card[j] + card[k]);
+                }
+            }
+        }
+        let setToArr = [...temp].sort((a, b)=>b-a);
+        answer = setToArr[k-1];
+
+        return answer;
+    }
+
+    let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+    console.log(solution(10, 3, arr));
+})();
