f63124f94e91755c34324ee4a70f72bf8e778012
nggoong <nggoong@naver.com>
2022-04-24 15:38:16 +0900

    괄호 문자 제거 문제 다른 알고리즘으로 해결✔

--- StackQueue/RemoveBracketString.js
+++ StackQueue/RemoveBracketString.js
@@ -10,13 +10,33 @@
                 while(store.pop()!=='(');
             }
             else store.push(x);
-            console.log(store.join(''));
         }
         answer = store.join('');
 
         return answer;
     }
 
+    function solution2(str) {
+        let answer;
+        let store = [];
+
+        for(let x of str) {
+            if(x!==')') {
+                store.push(x);
+            }
+            else if(x === ')') {
+                while(store[store.length - 1] !== '(') {
+                    store.pop();
+                }
+                store.pop();
+            }
+        }
+        answer= store.join('');
+
+        return answer;
+    }
+
     let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
     console.log(solution(str));
+    console.log(solution2(str));
 })();
