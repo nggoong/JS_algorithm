cc6d5ab4670424cd048ce292c66d5a7090cbc253
nggoong <nggoong@naver.com>
2022-04-26 14:52:36 +0900

    공주구하기 문제 배열 초기화 부분 수정✔

--- StackQueue/SavePrincess.js
+++ StackQueue/SavePrincess.js
@@ -3,30 +3,26 @@
 (()=> {
     function solution(n, m) {
         let answer;
-        let arr = [];
+        let arr = Array.from({length:8}, (value, index)=>index + 1);
         let count = 0;
 
-        for(let i = 1; i <= n; i++) {
-            arr.push(i);
-        }
 
         while(arr.length !==1) {
             count++;
             if(count !== 3) {
                 let temp = arr.shift();
                 arr.push(temp);
-                console.log(arr)
             }
             else if(count === 3) {
                 arr.shift();
                 count = 0;
-                console.log(arr)
             }
             
         }
         answer = arr[0];
         return answer;
     }
+
 
     console.log(solution(8, 3));
 })();
